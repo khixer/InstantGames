08136bd598bcee430cb130ca6864277aa6ed9064
khixer <muhammad.khizar@tintash.com>
2018-07-19 02:17:01 +0500

    Remove comments and add TODO's

--- sel.py
+++ sel.py
@@ -4,15 +4,13 @@ import time
 
 
 def parse_games(html):
-    all_games = html.find_elements_by_class_name("ellipsis")
     names = []
-    for i in all_games:
+    for i in html:
         names.append(i.text)
 
-    game_name = []
-    makers = []
-    number_of_users = []
+    game_name, makers, number_of_users = [], [], []
 
+    # TODO: need to extract the count of users
     for i, val in enumerate(names):
         if i % 2 == 0:
             game_name.append(names[i])
@@ -24,37 +22,33 @@ def parse_games(html):
 
 driver = webdriver.Chrome()
 driver.get('https://www.facebook.com/instantgames/')
-
-print driver.title
-# html = driver.find_element_by_xpath(".//html")
+time.sleep(10)
 
 # doing infifnte scrolling
-count = 0
+scroll_count = 0
 
 all_pages = ''
+prev_game, prev_maker = [], []
 while True:
-    driver.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(4)
-    html_source = driver.find_element_by_xpath(".//html")
-    count += 1
-    print(count)
+    html_source = driver.find_elements_by_class_name('ellipsis')
+    scroll_count += 1
+    print(scroll_count)
     if html_source:
-        # TODO: function to parse the page
-        # TODO: check if result of previous data = new scraped data break the
-        # loop.
-        all_pages = html_source
-    else:
-        break
+        # function to parse the page
+        game, maker = parse_games(html_source)
+        # TODO: Wrtie to csv here
+        print "\n\n\n\n"
+        print("--------game_name------------")
+        print game
+        print("--------makers------------")
+        print maker
+        # TODO: if result of previous data = new scraped data break the loop
+        if game != prev_game and maker != prev_maker:
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(4)
+        else:
+            break
 
-
-# game, maker = parse_games(all_pages)
-
-
-# print "\n\n\n\n"
-# print("--------game_name------------")
-# print game
-# print("--------makers------------")
-# print maker
 
 driver.quit()
